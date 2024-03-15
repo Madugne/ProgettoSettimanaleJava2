1821e5e4ef4565a251c9f48defe60a35e7525c6e
Fabio Yu <fabioyu7665@gmail.com>
2024-03-15 12:44:21 +0100

    Aggiunta metodi inventario, da testare

---
+++ src/main/java/chunyinyu/entities/Inventario.java
@@ -0,0 +1,26 @@
+package chunyinyu.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Inventario {
+    private List<Elemento> catalogo;
+    public Inventario() {
+        catalogo = new ArrayList<>();
+    }
+    public void aggiungiElemento(Elemento elemento) {
+        catalogo.add(elemento);
+    }
+    public void rimuoviElemento(String isbn) {
+        catalogo.removeIf(rimuovi -> rimuovi.getIsbn().equals(isbn));
+    }
+    public Elemento ricercaIsbn(String isbn) {
+        return catalogo.stream().filter(ricercaIsbn -> ricercaIsbn.getIsbn().equals(isbn)).findFirst().orElse(null);
+    }
+    public List<Elemento> ricercaAnnoPubblicazione(int anno) {
+        return catalogo.stream().filter(ricercaAnno -> ricercaAnno.getAnnoPubblicazione() == anno).toList();
+    }
+    public List<Elemento> ricercaPerAutore(String autore) {
+        return catalogo.stream().filter(ricercaAutore -> ricercaAutore instanceof Libri).filter(libro -> ((Libri) libro).getAutore().equals(autore)).toList();
+    }
+}
