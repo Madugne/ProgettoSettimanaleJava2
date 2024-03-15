de779afbdfca219ce44151dbab1c0b0c8090282b
Fabio Yu <fabioyu7665@gmail.com>
2024-03-15 16:44:24 +0100

    Testato

--- src/main/java/chunyinyu/Application.java
+++ src/main/java/chunyinyu/Application.java
@@ -6,8 +6,6 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Application {
-
-    private static final String FILE_NAME = "INVENTARIO.txt";
     public static void main(String[] args) {
         Inventario inventario = new Inventario();
         Scanner scanner = new Scanner(System.in);
